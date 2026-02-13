68c30fd202d345db6170392ce76e4a44dee00bfe
AZADAGROUP1 <michalmotykapl@gmail.com>
2026-02-13 15:44:31 +0100

    one

--- azada_wholesaler_pro/controllers/admin/AdminAzadaParentController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class AdminAzadaParentController extends ModuleAdminController {
-    public function __construct() {
-        $this->bootstrap = true;
-        parent::__construct();
-        // Przekierowanie do pierwszej zakładki po kliknięciu w nagłówek
-        Tools::redirectAdmin($this->context->link->getAdminLink('AdminAzadaWholesaler'));
-    }
-}
--- azada_wholesaler_pro/views/templates/admin/configure.tpl
+++
@@ -1,7 +0,0 @@
-<div class="panel">
-    <h3><i class="icon icon-tags"></i> Integracja AZADA PRO</h3>
-    <p>
-        <strong>Moduł jest gotowy do pracy.</strong><br />
-        Użyj menu po lewej stronie "Integracja PRO (AZADA)", aby dodać hurtownie.
-    </p>
-</div>
